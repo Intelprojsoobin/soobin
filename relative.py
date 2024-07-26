a01e2add61482132312b5620ec9fd8972f6f1f63
soobin1113 <wjdtnqls92@naver.com>
2024-07-26 14:39:10 +0900

    Add files via upload

---
+++ relative.py
@@ -0,0 +1,11 @@
+first = int(input("첫번째 정수 : "))
+second = int(input("두번째 정수 :"))
+
+print("{} > {} = {}".format(first,second,(first>second)))
+print("{} >= {} = {}".format(first,second,(first>=second)))
+print("{} < {} = {}".format(first,second,(first<second)))
+print("{} <= {} = {}".format(first,second,(first<=second)))
+print("{} == {} = {}".format(first,second,(first==second)))
+print("{} != {} = {}".format(first,second,(first!=second)))
+#비교 혹은 관계 연산자
+#두 수의 대소 동등 관계를 계산하여 논리값을 내어주는 연산
